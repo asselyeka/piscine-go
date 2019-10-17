9404775ffcf8c1883c996279cd5321e7c83d873b
asselyeka <a.moldasheva@gmail.com>
2019-10-17 14:48:23 +0600

    added quest3-8 v1

---
+++ strrev.go
@@ -0,0 +1,22 @@
+package piscine
+
+func StrRev(s string) string {
+	var count int
+	var a int
+	for index, word := range s {
+		if word == 233 {
+			a = -1
+		}
+		count = index + a
+	}
+	str := s
+	revstr := []byte(str)
+	j := 0
+	for i := count; i >= 0; i-- {
+		revstr[i] = byte(s[j])
+		j++
+	}
+	finalstr := string(revstr)
+
+	return finalstr
+}
