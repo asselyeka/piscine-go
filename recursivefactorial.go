8a5f2b7577dd1e6817d64c92068da3033e9fa0f3
asselyeka <a.moldasheva@gmail.com>
2019-10-21 12:09:23 +0600

    updated

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,15 +1,15 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 || nb > 50 {
-		return 0
-	} else {
+	if nb > 0 || nb < 50 {
 		if nb == 1 {
 			return 1
 		}
 		if nb > 1 {
 			return nb * RecursiveFactorial(nb-1)
 		}
+		return 0
+	} else {
 		return 0
 	}
 }
