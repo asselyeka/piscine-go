91e5f7c72d1eb1a5122b11ee87bcd772b1164343
asselyeka <a.moldasheva@gmail.com>
2019-10-21 12:06:16 +0600

    updated

--- recursivefactorial/test.go
+++ recursivefactorial/test.go
@@ -9,4 +9,8 @@ import (
 func main() {
 	arg := 5
 	fmt.Println(piscine.RecursiveFactorial(arg))
+	arg1 := -4
+	fmt.Println(piscine.RecursiveFactorial(arg1))
+	arg2 := 0
+	fmt.Println(piscine.RecursiveFactorial(arg2))
 }
