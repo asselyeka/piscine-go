0e9d1abe4b9f008140047f55e2def338025f8566
asselyeka <a.moldasheva@gmail.com>
2019-10-23 11:52:35 +0600

    added q6-3

---
+++ revparams/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	len := 0
+	for index := range arguments {
+		len = index + 1
+	}
+	for i := len - 1; i > 0; i-- {
+		strAsRune := []rune(arguments[i])
+		for _, letter := range strAsRune {
+			z01.PrintRune(letter)
+		}
+		z01.PrintRune('\n')
+	}
+}
